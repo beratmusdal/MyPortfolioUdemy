b2b824a0367c64b0491dce3e414812c53c842a70
Berat Muşdal <beratmsdl@gmail.com>
2024-07-22 22:11:19 +0300

    22.07 update

---
+++ MyPortfolioUdemy/Migrations/20240722092917_mig2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPortfolioUdemy.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoLists");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoLists",
+                columns: table => new
+                {
+                    ToDoListId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoLists", x => x.ToDoListId);
+                });
+        }
+    }
+}
---
+++ MyPortfolioUdemy/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortolioUdemy.DAL.Context;
+
+namespace MyPortolioUdemy.ViewComponents.LayoutViewComponents
+{
+	public class _LayoutNavbarComponentPartial:ViewComponent
+	{
+
+		public IViewComponentResult Invoke()
+		{
+
+			return View();
+		}
+	}
+}
